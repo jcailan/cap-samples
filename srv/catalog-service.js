4f8ccd846ca36a421a81c3ce04922d51dca1029f
Jhodel Cailan <jhodel.cailan@gmail.com>
2020-07-27 03:15:00 +0800

    Consume External Service - Part 3

--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -1,9 +1,11 @@
 const cds = require('@sap/cds');
+const cdse = require('cdse');
 
 module.exports = cds.service.impl(async function() {
 	const { Products } = this.entities;
-	const service = await cds.connect.to('NorthWind');
-	this.on('READ', Products, request => {
-		return service.tx(request).run(request.query);
+	const service = await cdse.connect.to('NorthWind');
+	this.on('READ', Products, async () => {
+		const result = await service.run({ url: 'Products' });
+		return result.value;
 	});
 });
