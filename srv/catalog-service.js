0a816532a7901d0425541e23d9c7861096af9b21
Jhodel Cailan <jhodel.cailan@gmail.com>
2022-11-09 16:37:35 +0800

    Generated new cap project

--- srv/catalog-service.js
+++
@@ -1,9 +0,0 @@
-const cds = require('@sap/cds');
-
-module.exports = cds.service.impl(async function() {
-	const { Products } = this.entities;
-	const service = await cds.connect.to('NorthWind');
-	this.on('READ', Products, request => {
-		return service.tx(request).run(request.query);
-	});
-});
